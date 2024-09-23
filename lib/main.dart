940ece69205f66d88bc24d34c31c521386aa8f26
KshitijH123 <kshitijhapase@gmail.com>
2024-09-23 18:00:51 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+
+import 'package:flutter/material.dart';
+import 'package:sunflower/sunflower_widget.dart';
+
+const int maxSeeds = 250;
+
+void main() {
+  runApp(const Sunflower());
+}
+
+class Sunflower extends StatefulWidget {
+  const Sunflower({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return _SunflowerState();
+  }
+}
+
+class _SunflowerState extends State<Sunflower> {
+  int seeds = maxSeeds ~/ 2;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: const AppBarTheme(elevation: 2),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Sunflower'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: SunflowerWidget(seeds),
+              ),
+              const SizedBox(height: 20),
+              Text('Showing ${seeds.round()} seeds'),
+              SizedBox(
+                width: 300,
+                child: Slider(
+                  min: 1,
+                  max: maxSeeds.toDouble(),
+                  value: seeds.toDouble(),
+                  onChanged: (val) {
+                    setState(() => seeds = val.round());
+                  },
+                ),
+              ),
+              const SizedBox(height: 20),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
